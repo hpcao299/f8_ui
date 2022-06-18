9b2297ee37b4a2de6404248a5afaac3148801621
HPhucJ2007 <caohoangphuc2909@gmail.com>
2022-06-18 21:39:03 +0700

    header ui #1

--- src/layouts/components/Header/Header.js
+++ src/layouts/components/Header/Header.js
@@ -1,5 +1,22 @@
+import classNames from 'classnames/bind';
+import images from '~/assets/images';
+import styles from './Header.module.scss';
+
+const cx = classNames.bind(styles);
+
 function Header() {
-    return <div>Header</div>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('logo')}>
+                <img src={images.logo} alt="F8" />
+                <h4 className={cx('logo-heading')}>Học Lập Trình Để Đi Làm</h4>
+            </div>
+
+            <div className={cx('body')}></div>
+
+            <div className={cx('actions')}></div>
+        </header>
+    );
 }
 
 export default Header;
